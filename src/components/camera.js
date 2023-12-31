6ef7d85f7a10043489b75a7bf4fb12fb30930e28
Mario <mh.torneria@gmail.com>
2023-12-31 15:57:55 -0300

    ajustes de la cámara, limpieza del código

--- src/components/camera.js
+++ src/components/camera.js
@@ -1,43 +1,68 @@
+import '../App.css'
 import React, { Component } from "react";
 import QrReader from 'react-qr-scanner'
-
+let textResult = 'Nada escaneado';
 
 class QrContainer extends Component {
     constructor(props){
         super(props);
         this.state={
-            result:'Manten el QR en la pantalla'
+            result:'Manten el QR en la pantalla',
+            isCamaraActive: this.props.isCameraActive // Usar directamente this.props.isCameraActive en el estado
         }
-        this.handleScan = this.handleScan.bind(this)
+        this.handleScan = this.handleScan.bind(this);
     }
 
     handleScan(result){
         this.setState({
             result: result
         });
+        
+        if(result){
+            textResult = result.text;
+            console.log(textResult);
+        }
+
+        return result;
     }
 
     handleError(err){
         console.error(err)
     }
 
+    toggleCamera = () =>{
+        this.setState(prevState =>({
+            isCamaraActive: !prevState.isCamaraActive
+        }))
+    }
+
     render(){
         return(
             <React.Fragment>
-                <div>
-                    <QrReader
-                        delay={100}
-                        onError={this.handleError}
-                        onScan={this.handleScan}
-                    />
+                <button className="btn-cam" onClick={this.toggleCamera}>
+                        {this.state.isCamaraActive ? 'Apagar Cámara' : 'Encender Cámara'}
+                </button>
+                <div className="cam-space">
+                    {this.state.isCamaraActive && (
+                        <QrReader
+                            delay={100}
+                            onError={this.handleError}
+                            onScan={this.handleScan}
+                        />
+                    )}
                 </div>
-                <p>
-                    {this.state.result}
-                </p>
 
+                <p className="codigo-escaneado">
+                    {textResult}
+                </p>
             </React.Fragment>
         )
     }
 }
 
 export default QrContainer;
+
+/*                    <button onClick={this.toggleCamera}>
+                        {this.state.isCamaraActive ? 'Apagar Cámara' : 'Encender Cámara'}
+                    </button>
+ */
